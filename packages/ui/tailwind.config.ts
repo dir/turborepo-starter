e7c696b0b74e5fc251b1ff7a71056c9c8948a72b
Luke Davis <luke@finesse.host>
2025-01-16 17:41:09 -0600

    additional cleanup

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -4,7 +4,10 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
   darkMode: ["class"],
-  content: ["src/**/*.{ts,tsx,js,jsx}", "../../packages/ui/src/components/**/*.{ts,tsx}"],
+  content: [
+    "src/**/*.{ts,tsx,js,jsx}",
+    "../../packages/ui/dist/components/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -50,6 +53,28 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/**/*.{ts,tsx}"],
   format: ["esm"],
-  splitting: true,
   sourcemap: true,
   clean: true,
   dts: true,
+  minify: true,
 });
