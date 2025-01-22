6cbcddc4c28316d3c5279b13f1d21c8b0219a1e2
Luke Davis <luke@finesse.host>
2025-01-22 00:00:03 -0600

    add type to eslint cfg

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,5 @@
 import baseConfig from "@repo/eslint-config";
 import nextJsConfig from "@repo/eslint-config/nextjs";
 
+/** @type {import('typescript-eslint').Config} */
 export default [...baseConfig, ...nextJsConfig];
